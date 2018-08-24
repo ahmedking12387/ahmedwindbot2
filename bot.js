d6a839e4bfe59ab290b0e5e1293d7a8486361fd4
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-24 10:25:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -222,21 +222,44 @@ client.user.setGame(args , '');
 
 
 
+client.on('message',function(message) {
+const prefix = '+'
+	if (message.author.x5bz) return;
+  if (!message.content.startsWith(prefix)) return;
 
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
+  let args = message.content.split(" ").slice(1);
+	
+  if (command == "mute") {
+  let rank = message.guild.member(message.author).roles.find('name', 'clear');
+  if (!rank) return message.reply('انت لا تمتلك الرتبه المخصصه لهذا الامر')
+      message.delete();
 
-
-
-
-
-
-
-
-
-
-
-
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+    let messageArray = message.content.split(' ');
+    let muteRole = message.guild.roles.get('482449634552315916') || message.guild.roles.find('name', 'Muted');
+    let muteMember = message.mentions.members.first();
+    let muteReason = messageArray[2];
+    let muteDuration = messageArray[3];
+       if(!muteRole) return message.guild.createRole({name: 'Muted'}).then(message.guild.channels.forEach(chan => chan.overwritePermissions(muteRole, {SEND_MESSAGES:false,ADD_REACTIONS:false})));
+       if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة مني``');
+       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
+       if(!muteReason) return message.channel.send('ℹ **Error:** ``حدد سباّ``');
+       if(!muteDuration) return message.channel.send('ℹ **Error:** ``حدد وقت زمني``');
+       if(!muteDuration.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('ℹ **Error:** ``حدد وقت زمني صحيح``');
+       message.channel.send(`:white_check_mark: **تم اعطاء العضو ميوت : ${muteMember}**`);
+       muteMember.addRole(muteRole);
+       muteMember.setMute(true)
+       .then(() => { setTimeout(() => {
+           muteMember.removeRole(muteRole)
+           muteMember.setMute(false)
+       }, mmss(muteDuration));
+       });
+   } 
+});
 
 
 

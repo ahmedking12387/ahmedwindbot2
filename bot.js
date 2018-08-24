05d37cd3f5dc47eb2dc381ea7a5fe964545bc6f9
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-24 07:17:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -352,7 +352,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
 
 
 
-hero.on('guildMemberAdd',async member => {
+client.on('guildMemberAdd',async member => {
   const Canvas = require('canvas');
   const jimp = require('jimp');
   const w = ['./welcome_4.png'];

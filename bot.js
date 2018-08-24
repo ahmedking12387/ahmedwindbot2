b3fb5f8a8f509529d36fd225900b4971c00675cf
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-24 10:04:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,22 +30,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-    if(!message.channel.guild) return;
-if (message.content.startsWith('+ping')) {
-if(!message.channel.guild) return;
-var msg = `${Date.now() - message.createdTimestamp}`
-var api = `${Math.round(client.ping)}`
-if (message.author.bot) return;
-let embed = new Discord.RichEmbed()
-.setAuthor(message.author.username,message.author.avatarURL)
-.setColor('RANDOM')
-.addField('**Time Taken:**',msg + " ms :signal_strength: ")
-message.channel.send({embed:embed});
-}
-});
-
-
 
 
 	

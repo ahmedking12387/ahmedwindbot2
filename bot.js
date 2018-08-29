d3695144fd4a19cefdc4b59eae40ae4f29c595f1
ahmedking12387 <33984820+ahmedking12387@users.noreply.github.com>
2018-08-29 10:46:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -373,46 +373,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
 
 
 
-  client.on('message' , async (message) => {
-var prefix = "+"
-    if(message.content.startsWith(prefix + "topinvite")) {
-if(message.author.bot) return;
-if(!message.channel.guild) return message.reply(' Error : \` Guild Command \`');
-  var invites = await message.guild.fetchInvites();
-    invites = invites.array();
-    arraySort(invites, 'uses', { reverse: true });
-    let possibleInvites = ['User Invited |  Uses '];
-    invites.forEach(i => {
-        if (i.uses === 0) { 
-            return;
-        }
-      possibleInvites.push(['\n\ ' +'<@'+ i.inviter.id +'>' + '  :  ' +   i.uses]);
-      if (i.uses === 1) {//يمديك تعدل رقم وصول العدد حق الانفايت الى اأقل أو أكثر
-          message.member.addRole(message.member.guild.roles.find("name","Vital"))//هنآ أسم ألرتبه اللي تجيهه
-.catch(RebeL =>{
-console.log('`Error`: ' + RebeL);
-});
-}
-if (i.uses === 1) {
-message.member.addRole(message.member.guild.roles.find("name","Vital"))
-.catch(RebeL =>{
-console.log('`Error`: ' + RebeL);
-});
-}
-if (i.uses === 1) {
-message.member.addRole(message.member.guild.roles.find("name","Vital"))
-.catch(RebeL =>{
-console.log('`Error`: ' + RebeL);
-});
-      }//معلومه بسيطه يمديك تكرر العمليهه أكثر من مره
-    })
-    const embed = new Discord.RichEmbed()
- .setColor('#36393e')
-    .addField("Top Invites." ,`${(possibleInvites)}`)
-
-    message.channel.send(embed)
-    }
-});
+  
 
 
 
